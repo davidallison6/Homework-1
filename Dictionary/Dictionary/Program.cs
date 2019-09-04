76d1cf44210c45ddd35cbdaaacb99cfebd453c52
David Allison <davidallison@ou.edu>
2019-09-03 21:23:08 -0500

    dictionary finished

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            double output = 0;
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+            dict.Add("apples", 0.99);
+            dict.Add("oranges", 0.50);
+            dict.Add("bananas", 0.50);
+            dict.Add("grapes", 2.99);
+            dict.Add("blueberries", 1.99);
+
+            Console.Write("Enter the desired item to display the price (apples, oranges, bananas, grapes, blueberries): ");
+            userInput = Console.ReadLine();
+
+            if (dict.TryGetValue(userInput, out output))
+            {
+                Console.WriteLine($"The price of {userInput} is {output:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
